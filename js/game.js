055773ca1a5d5b61fcb0ef0b33ee65cef01ffaae
Javier Rocha Gómez <javierrg1992@gmail.com>
2023-11-02 12:38:57 +0100

    mcahine choose a cell around a flame

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
 //VARIABLES
+
 //global variables to count how many shoot made each player
 let enemyShoots = 0
 let enemyShootsOnTarget = 0
@@ -6,17 +7,18 @@ let playerShoots = 0
 let playerShootsOnTarget = 0
 //global variable for turn management
 let turnPlayer = true
-//global variable to storage cell on shoot
-const getCellByClass = (cellClass) => {
-    return Array.from(actionZone__ships.children).filter((cell) => cell.classList.contains(cellClass))
-}
+//golbal varable to storage first ship position for machine intelligence
+let firstPosition = null
+//gloabal vairbales araay to storage letter and numbers corerponding to cell positions
+const letters = ['a','b','c','d','e','f','g','h','i','j']
+const numbers = [1,2,3,4,5,6,7,8,9,10]
 /**
  * function to filter tagName of event.target to call or not function 
  * to generate an animation
  * 
  * @param {event} event 
  */
-const shoot = (event = false, cellClass) => {
+const shoot = (event = false, cellId) => {
     if(event){
         const e = event.target
         // console.log('aqui es donde pulso '+e.classList)
@@ -29,38 +31,159 @@ const shoot = (event = false, cellClass) => {
         }
     }else{
         Array.from(actionZone__ships.children).forEach((cell) => {
-            if(cell.classList.contains(cellClass))
-            generateAnimation(cell)
+            if(cell.getAttribute('id') == cellId){
+                generateAnimation(cell)
+                cell.classList.add('disabled')
+            }
         })
         turnPlayer = true
     }
 }
-const randomPosition = () => {
-    return postionBall[Math.floor(Math.random() * postionBall.length)]
+/**
+ * function to return an element which classList matches position parameter
+ * 
+ * @param {string} position 
+ * @returns HTMLDIVELEMENT
+ */
+const getCellByPosition = (position) => {
+    for (const element of Array.from(actionZone__ships.children)) {
+        if(element.classList.contains('p'+position))
+        return  element
+    }
 }
+/**
+ * function to get a specific cells by id given as parameter
+ * 
+ * @param {string} cellId 
+ * @param {HTMLAllCollection} cells 
+ */
+const getCellById = (cellId,cells) => {
+    console.log('entro en la funcion de id')
+    for(const cell of Array.from(cells)){
+        if(cell.id == cellId){
+            return cell
+        }
+    }
+}
+/**
+ * function to generate a random position to shoot while this postision is not choosen yet
+ * 
+ * @returns string
+ */
+const randomPosition = () => {
+    let randomPosition = 0
+    let cell
+    let position
+    do{
+        randomPosition = Math.floor(Math.random() * positionBall.length)
+        position = positionBall[randomPosition]
+        cell = getCellByPosition(position)
+        console.log(cell.classList)
+    }while(cell.classList.contains('disabled'))
+
+    return positionBall[randomPosition]
+}
+/**
+ * function to detect any ship in board by look into its classList
+ * 
+ * @returns string
+ */
 const detectShip = () => {
     for (const cell of Array.from(actionZone__ships.children)) {
         if(cell.classList.contains('cell')){
             const son = cell.firstElementChild
-            if(son.classList.contains('ballShootWhite')){
-                return cell.id.substring(1)
+            if(son.classList.contains('ballShootRed')){
+                return cell.id
             }
         }
     }
     return null
 }
-// const setPosition = (position) => {
-//     const letter = ['a','b','c','d','e','f','g','h','i','j']
+const changeId = (itemToChoose,cellId) => {
+    let positionItem
+    let randomPos 
+    let auxArray
+    let newId
+    console.log('este es el elemento que voy a cmabiar '+itemToChoose)
+    if(isNaN(itemToChoose)){
+        auxArray = [...letters]
+        console.log('cambio la letra')
+    }else{
+        auxArray = [...numbers]
+        console.log('cambio el numero')
+    }
+    positionItem = auxArray.indexOf(itemToChoose)
+    console.log('esta es la poscion del item dentro del array '+positionItem)
+    do{
 
-// }
+        do{
+            randomPos = Math.floor(Math.random() * 2 - 1)
+        }while((positionItem == 0 && randomPos == -1) || (positionItem == 9 && randomPos == 1))
+        console.log('esta es la posicion del nuevo elemento '+(positionItem + randomPos))
+        // console.log(' esta es la posicion de la letra '+positionItem)
+        // console.log(' este es el numero aleatorio '+randomPos)
+        // console.log('esta es la nueva letra que se escoge al azar '+letters[(positionItem + randomPos)])
+        if(isNaN(itemToChoose)){
+            newId = '_p' + letters[(positionItem + randomPos)] + cellId.substring(3)
+        }else{
+            newId = '_p' + cellId.substring(2,3) + numbers[(positionItem + randomPos)]
+        }
+    }while(cellId == newId)
+    console.log('este es el id que le paso por parametro '+cellId)
+    console.log('este es el nuevo id que genero a cambiar un parametro del original '+newId)
+    return newId
+}
+/**
+ * function to choose waht element form id change for next shoot to shoot every posibilities the ship is around the flame
+ * 
+ * @param {string} position id from cell with a flame on it
+ * @returns number or letter from this id dpending of random choice
+*/
+const chooseIdRandom = (position) =>{
+    //get a random number to choose between 2 option by randomly choice
+    random = Math.floor(Math.random() * 1)
+    //conditional to return only letter
+    if(random == 0){
+        return position.substring(2,3)
+    }
+    //conditional to return only number
+    else{
+        return position.substring(3)
+    }
+}
+const setPosition = (cellId) => {
+    //declare varible to storage target cell in it to compare later
+    let cellTarget
+    let newId
+    if(!firstPosition){
+        firstPosition = cellId 
+    }
+    do{
+        newId = changeId(chooseIdRandom(cellId),cellId)
+        console.log('este es el id nuevo cambiando uno de los parametros '+newId)
+        cellTarget = getCellById(newId,actionZone__ships.children)
+        console.log('esta es la celda objetivo '+cellTarget.id)
+        console.log('esta son las clases de la celdd objetivo '+cellTarget.classList)
+    }while(cellTarget.classList.contains('disabled'))
+    return cellTarget.id
+}
+/**
+ * function to emulate machine intelligence by calling other function
+ */
 const machineTurn = () => {
-    // let positionShoot = detectShip()
-    // if(positionShoot){
-    //     // setPosition(positionShoot)
-    // }else{
-        positionShoot = randomPosition()
-    // }
-    shoot(false,'p'+positionShoot)
+    //calling method to get id from cell with a flame on it
+    let positionShoot = detectShip()
+    console.log('esta es la celda donde se ha detectado un barco '+positionShoot)
+    //callig method setPosition() if positionShip return something
+    if(positionShoot != null){
+        //calling fucntion to choose a position where a ship may be in, around the flame
+        positionShoot = setPosition(positionShoot)
+        console.log('esta es la celda donde quiero disparar alrrededor del barco '+positionShoot)
+    }else{
+        //calling method to get a random positoin if there are not shootable ships in board 
+        positionShoot ='_p' + randomPosition()
+    }
+    shoot(false,positionShoot)
 }
 
 //events
--- js/loadGame.js
+++ js/loadGame.js
@@ -1,18 +1,18 @@
 const infoZone__ships = document.getElementById('infoZone__ships')
 const actionZone__ships = document.getElementById('actionZone__ships')
 let iterator = 0
-const postionBall = [
-'a1','a2','a3','a4','a5','a6','a7','a8','a9','a10',
-'b1','b2','b3','b4','b5','b6','b7','b8','b9','b10',
-'c1','c2','c3','c4','c5','c6','c7','c8','c9','c10',
-'d1','d2','d3','d4','d5','d6','d7','d8','d9','d10',
-'e1','e2','e3','e4','e5','e6','e7','e8','e9','e10',
-'f1','f2','f3','f4','f5','f6','f7','f8','f9','f10',
-'g1','g2','g3','g4','g5','g6','g7','g8','g9','g10',
-'h1','h2','h3','h4','h5','h6','h7','h8','h9','h10',
-'i1','i2','i3','i4','i5','i6','i7','i8','i9','i10',
-'j1','j2','j3','j4','j5','j6','j7','j8','j9','j10',
-]
+let positionBall = [
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", "i1", "j1",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "i2", "j2",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "i3", "j3",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", "i4", "j4",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", "i5", "j5",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", "i6", "j6",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "i7", "j7",
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", "i8", "j8",
+    "a9", "b9", "c9", "d9", "e9", "f9", "g9", "h9", "i9", "j9",
+    "a10", "b10", "c10", "d10", "e10", "f10", "g10", "h10", "i10", "j10"
+];
 
 //GLOBAL VARIABLED
 
@@ -61,14 +61,14 @@ const loadCells = () => {
         for (let j = 0; j < 10; j++) {
             position = ( i * 10 ) + j
             const cell1 = createCell(i+1,j+1)
-            cell1.classList.add('e'+postionBall[position])
-            cell1.id = '_e'+postionBall[position]
-            cell1.appendChild(createElement('DIV','amunnition', 'ballEnemyOutside','e'+postionBall[position]))
+            cell1.classList.add('e'+positionBall[position])
+            cell1.id = '_e'+positionBall[position]
+            cell1.appendChild(createElement('DIV','amunnition', 'ballEnemyOutside','e'+positionBall[position]))
             fragment1.appendChild(cell1)
             const cell2 = createCell(i+1,j+1)
-            cell2.classList.add('p'+postionBall[position])
-            cell2.id = '_p'+postionBall[position]
-            cell2.appendChild(createElement('DIV','amunnition','e'+postionBall[position]))
+            cell2.classList.add('p'+positionBall[position])
+            cell2.id = '_p'+positionBall[position]
+            cell2.appendChild(createElement('DIV','amunnition','p'+positionBall[position]))
             fragment2.appendChild(cell2)
         }
     }
